0bbbcddfa4f608c60739e9369f4dfbfab342c6dc
Angor78 <angor78@gmail.com>
2022-06-18 12:33:56 +0300

    add Nav Component and css file

---
+++ src/nav/Nav.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Nav.module.css'
+
+export const Nav = () => {
+
+  return(
+    <ul className={s.nav}>
+      <li>link 1</li>
+      <li>link 2</li>
+      <li>link 3</li>
+      <li>link 44</li>
+    </ul>
+  )
+}
