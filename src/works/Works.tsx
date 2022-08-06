a0c660f273e29f80d941917491941a1732c453ad
Angor78 <angor78@gmail.com>
2022-08-06 20:41:59 +0300

    works block start

--- src/works/Works.tsx
+++ src/works/Works.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import s from './Works.module.css'
 import styleContainer from "../common/styles/Container.module.css";
 import {Work} from "./work/Work";
+import img1  from './../assets/image/1.jpeg'
+import img4  from './../assets/image/4.png'
 
 export const Works = () => {
   return(
@@ -9,9 +11,14 @@ export const Works = () => {
       <div className={`${styleContainer.container} ${s.worksContainer}`}>
         <h2 className={s.title}>My works</h2>
         <div className={s.works}>
-          <Work title={'Todolist'}/>
-          <Work title={'Samurai way'}/>
-        </div>
+
+            <Work title={'Todolist'} poster={img1}/>
+            <Work title={'Samurai way'} poster={img4}/>
+
+            <Work title={'Todolist'} poster={img4}/>
+            <Work title={'Samurai way'} poster={img1}/>
+
+          </div>
       </div>
     </div>
   )
--- src/works/work/Work.tsx
+++ src/works/work/Work.tsx
@@ -2,21 +2,27 @@ import React from "react";
 import s from './Work.module.css'
 
 
-
-export const Work = (props: any) => {
+type WorkPropsType = {
+  poster: string
+  title: string
+}
+export const Work = (props: WorkPropsType) => {
   return (
     <div className={s.work}>
-      <div className={s.img}>
-        <button>Present</button>
+      <div className={s.poster}>
+        <img src={props.poster} alt="poster" width={'100%'} height={'100%'}/>
+        <div className={s.description}>
+          <h3>{props.title}</h3>
+          <p>
+        Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit Lorem ipsum dolor sit amet, co
+        Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit Lorem ipsum dolor sit amet, co
+            <button>Demo</button>
+          <button>Git</button>
+           </p>
+        </div>
       </div>
-      <h3>{props.title}</h3>
-      <span className={s.description}>
-        Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit Lorem ipsum dolor sit amet, co
-        Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit Lorem ipsum dolor sit amet, co
-
-      </span>
     </div>
   )
 }
