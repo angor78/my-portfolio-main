4ca41ae361c3695fa10787c475bc40bda0876778
Angor78 <angor78@gmail.com>
2022-08-13 15:27:55 +0300

    add links and posters to works

--- src/works/Works.tsx
+++ src/works/Works.tsx
@@ -2,17 +2,31 @@ import React from 'react';
 import s from './Works.module.scss'
 import styleContainer from "../common/styles/Container.module.css";
 import {Work} from "./work/Work";
+import todosImg from './../assets/image/todos.png'
+import samuraiImg from './../assets/image/samurai-way.png'
 
 
 export const Works = () => {
+  let todosDesc = 'Stack: React/Redux/Redux-Toolkit/Storybook/Axios \n' +
+    'React-router-dom v6/ Jest(Unit-Tests) /Formik/Chakra UI .\n' +
+    'Todolist app created with react hooks, routing and\n' +
+    'redirects, chakra/material UI, REST api request/response flow, \n' +
+    'unit-tests and etc'
+  let samuraiDesc = 'Stack: React/Redux/TypeScript/Redux/Thunk/React-router-dom \n' +
+    'v6/Redux-form/ Axios/ Jest.\n' +
+    ' Social network app using react hooks, routing,working with\n' +
+    'REST api, login/logout flow, pagination, form and validation/file \n' +
+    'uploading using unit- tests for code workability check'
+  let todosLinks = ['https://angor78-todo-main.netlify.app/', 'https://github.com/angor78/todo-list-main']
+  let samuraiLinks = ['', 'https://github.com/angor78/samurai-way-main']
   return (
     <div className={s.worksBlock}>
       <div className={styleContainer.container}>
         <div className={s.worksContainer}>
           <h2>My works</h2>
           <ul className={s.cards}>
-            <Work title={'Todolist'}/>
-            <Work title={'Samuray-way'}/>
+            <Work title={'Todolist'} poster={todosImg} desc={todosDesc} links={todosLinks}/>
+            <Work title={'Samuray-way'} poster={samuraiImg} desc={samuraiDesc} links={samuraiLinks}/>
           </ul>
         </div>
       </div>
--- src/works/work/Work.tsx
+++ src/works/work/Work.tsx
@@ -4,18 +4,24 @@ import s from './Work.module.scss'
 
 type WorkPropsType = {
   title: string
+  poster: string
+  desc: string
+  links: Array<string>
 }
 export const Work = (props: WorkPropsType) => {
   return (<li className={s.cards__item}>
       <div className={s.card}>
-        <div className={s.card__image}></div>
+        <img className={s.card__image} src={props.poster} alt={'poster'}/>
         <div className={s.card__content}>
           <div className={s.card__title}>{props.title}</div>
-          <p className={s.card__text}>This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The
-            second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
+          <p className={s.card__text}>{props.desc}</p>
           <div className={s.buttonsWrap}>
-            <button>Demo</button>
-            <button>Git</button>
+            <a href={props.links[0]}>
+              <button>Demo</button>
+            </a>
+            <a href={props.links[1]}>
+              <button>Git</button>
+            </a>
           </div>
         </div>
       </div>
