d825a942526fa7e7c4870551c60e88574e292858
Angor78 <angor78@gmail.com>
2022-08-25 15:38:16 +0300

    dark theme fix

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -5,18 +5,21 @@ import {faChevronRight} from "@fortawesome/free-solid-svg-icons/faChevronRight";
 
 
 export const Nav = () => {
+
   const values = [
     {id: 1, text: "About me", link: '#main'},
     {id: 2, text: "Skills", link: '#skills'},
     {id: 3, text: "Works", link: '#works'},
     {id: 4, text: "Contacts", link: '#contacts'}
   ]
+
   const [activeId, setActiveId] = useState(1);
+
   return (
     <div className={s.nav}>
       <ul>
         {values.map((val) => (
-          <li onClick={() => setActiveId(val.id)}>
+          <li onClick={() => setActiveId(val.id)} key={val.id}>
             <a href={val.link} className={activeId === val.id ? 'Active' : "Inactive"}>{val.text} <FontAwesomeIcon
               icon={faChevronRight}/></a>
           </li>
